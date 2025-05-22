3706fe5d4cfca03b4d3a627d10568f4a3b5551bf
vais2004 <vkawale2004@gmail.com>
2025-05-22 18:48:20 +0530

    update header adding badge for items count  of wishlist and cart

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -7,12 +7,23 @@ const Navbar = () => {
   const navigate = useNavigate();
   const search = useSelector((state) => state.search.text);
 
-const handleSearchChange =(event)=>{
-  const searched=event.target.value;
-  dispatch(setSearchText(searched))
-  navigate('/products')
-}
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const totalCartQuantity = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
 
+  //const wishlistCount = useSelector((state) => state.wishlist.wishlistItems.length);
+  const wishlistCount = useSelector(
+    (state) => state.wishlist?.wishlistItems?.length || 0
+  );
+  console.log("Wishlist count in Navbar:", wishlistCount);
+
+  const handleSearchChange = (event) => {
+    const searched = event.target.value;
+    dispatch(setSearchText(searched));
+    navigate("/products");
+  };
 
   return (
     <header className="navbar navbar-expand-lg navbar-light bg-light">
@@ -31,8 +42,7 @@ const handleSearchChange =(event)=>{
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          
-            <div className=" col-lg-6 mx-auto my-2 my-lg-0">
+          <div className=" col-lg-6 mx-auto my-2 my-lg-0">
             <div className="input-group" style={{ maxWidth: "100%" }}>
               <span className="input-group-text" id="basic-addon1">
                 <i className="bi bi-search"></i>
@@ -44,9 +54,8 @@ const handleSearchChange =(event)=>{
                 value={search}
                 onChange={handleSearchChange}
               />
-              </div>
             </div>
-         
+          </div>
 
           <div className="navbar-nav ms-auto">
             <div className="btn-group" role="group">
@@ -54,12 +63,27 @@ const handleSearchChange =(event)=>{
                 Login
               </Link>
 
-              <Link to="/wishlist" className="nav-link px-3">
+              <Link
+                to="/wishlist"
+                className="nav-link pe-3 px-5 position-relative">
                 Wishlist <i className="bi bi-heart"></i>
+                <span
+                  className="position-absolute top-0 me-5 mt-2  start-100 translate-middle badge rounded-pill bg-danger"
+                  style={{
+                    fontSize: "0.7rem",
+                    transform: "translate(-50%, -50%)", // Explicit transform
+                  }}>
+                  {wishlistCount}
+                </span>
               </Link>
 
-              <Link to="/cart" className="nav-link px-3">
+              <Link to="/cart" className="nav-link pe-2 px-5 position-relative">
                 Cart <i className="bi bi-cart4"></i>
+                <span
+                  className="position-absolute top-0 me-5 mt-2 start-100 translate-middle badge rounded-pill bg-danger"
+                  style={{ fontSize: "0.7rem" }}>
+                  {totalCartQuantity}
+                </span>
               </Link>
             </div>
           </div>
